f7c4ee67c53867d21c7e2be05132a22071edaf0a
a.cianfarani <a.cianfarani@c2is.fr>
2013-09-06 14:05:56 +0200

    Add the update of skriv content before branch switch

--- editor/editor.php
+++ editor/editor.php
@@ -22,12 +22,13 @@ switch($_POST["action"]) {
         echo "Content saved into directory ".$_SESSION["language"]."/";
         break;
     case "push":
-
+        $skriv = file_get_contents("../".$language."/doc.skriv");
         $cmd = array();
         $cmd[] = "git add ../.";
         $cmd[] = "git commit -m'Auto commit from doc editor'";
-        $cmd[] = "git push origin master ";
+        $cmd[] = "git push origin master";
         $cmd[] = "git checkout gh-pages";
+        file_put_contents("../".$language."/doc.skriv",$skriv);
         $res = shell_exec(implode(";",$cmd));
         build($renderer,$_SESSION["language"]);
         $cmd = array();
